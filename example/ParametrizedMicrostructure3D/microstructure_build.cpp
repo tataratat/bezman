74575a0106cf075fd9074759ca46c5c17e6af00e
jzwar <zwar@ilsb.tuwien.ac.at>
2022-04-01 16:14:32 +0200

    Added unfinished 3D example for Microstructures

---
+++ example/ParametrizedMicrostructure3D/microstructure_build.cpp
@@ -0,0 +1,177 @@
+/*
+ * This Example illustrates, how the BezierManipulation suite can be used to
+ * build parametrized microstructures. It is also used to show the capabilities
+ * of deriving the structure with respect to the design variables, using
+ * algorithmic differentiation.
+ *
+ * Implementationwise, the code is structured as follows. There are three
+ * classes describing the geometry and the parametrization. The MicortileExample
+ * class, describes the parametrized Inner-spline, that is later inserted into
+ * the structure. It contains a function, that creates a microtile, based on a
+ * set of parameters. As the parameters are based on a function evaluation, it
+ * also provides the points, where this funciton is to be evaluated (in the case
+ * of a crosstile, these are the tips of the crosstile-branches). The
+ * DeformationFunctionExample class provides the outer geometry. As it is formed
+ * through a group of Bezier splines, it also holds segmentation information,
+ * i.e., how many splines are in each parametric dimension. This could also be
+ * generalized with a function, returning local coordinates based on the current
+ * index. The ValueFunction class finally provides the parametrization function,
+ * that returns the local Parameters, based on the position of the spline within
+ * the structure and a set of superordinate parameters. These three functions
+ * are then inserted as Template Arguments in the Microstructure-Generator
+ * class, which performes the compositions and derivations.
+ *
+ * The results are exported in xml format and can be plotted using the python
+ * scripts with gustav
+ *
+ */
+
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+#include "bezierManipulation/src/bezier_spline_group.hpp"
+#include "bezierManipulation/src/utils/computational_derivation/algo_diff_type.hpp"
+#include "bezierManipulation/src/utils/export.hpp"
+#include "cross_tile_3d.hpp"
+#include "microstructure_generator.hpp"
+#include "ring_segments_3d.hpp"
+
+using namespace beziermanipulation;
+
+/**
+ * @brief Value Field Function defined in the parametric domain of the
+ * deformation function
+ *
+ * This function is based on Algorithmic differentiation types, which
+ calculate
+ * the derivative of the value field function at the same time.
+ *
+ */
+class ValueFieldExample {
+ private:
+  using ADT = utils::computational_derivation::AlgoDiffType<double>;
+  using Point3D = Point<3, double>;
+
+ public:
+  // Number of super Parameters
+  static constexpr const std::size_t kNumberOfSuperParameters{12};
+
+  // Some super parameters
+  const std::array<ADT, kNumberOfSuperParameters> kSuperControlPoints{
+      ADT(0.1, kNumberOfSuperParameters, 0),
+      ADT(0.2, kNumberOfSuperParameters, 1),
+      ADT(0.1, kNumberOfSuperParameters, 2),
+      ADT(0.1, kNumberOfSuperParameters, 3),
+      ADT(0.1, kNumberOfSuperParameters, 4),
+      ADT(0.3, kNumberOfSuperParameters, 5),
+      ADT(0.1, kNumberOfSuperParameters, 6),
+      ADT(0.2, kNumberOfSuperParameters, 7),
+      ADT(0.1, kNumberOfSuperParameters, 8),
+      ADT(0.1, kNumberOfSuperParameters, 9),
+      ADT(0.1, kNumberOfSuperParameters, 10),
+      ADT(0.3, kNumberOfSuperParameters, 11)};
+
+  /**
+   * @brief Evaluate the Value field function
+   *
+   * Here, the value field function are the basis functions of a B-Spline
+   * interpolating between the SuperControlPoints. The B-Spline has a knot in
+   * the center, meaning that the basis functions have only local support.
+   */
+  template <std::size_t array_size>
+  std::array<ADT, array_size> Evaluate(
+      const std::array<Point3D, array_size>& evaluation_points) const {
+    // Initialize return value
+    std::array<ADT, array_size> evaluations{};
+    // Assign based on simple linear B-Spline basis
+    for (std::size_t i_point{}; i_point < array_size; i_point++) {
+      const double &x{evaluation_points[i_point][0]},
+          &y{evaluation_points[i_point][1]}, &z{evaluation_points[i_point][2]};
+      evaluations[i_point] =
+          (x < 0.5 ? (1 - 2. * x) : 0.) * (1 - y) * (1 - z) *
+              kSuperControlPoints[0] +
+          (x < 0.5 ? (2. * x) : (2. - 2. * x)) * (1 - y) * (1 - z) *
+              kSuperControlPoints[1] +
+          (x < 0.5 ? 0. : 2 * x - 1.) * (1 - y) * (1 - z) *
+              kSuperControlPoints[2] +
+          (x < 0.5 ? (1 - 2. * x) : 0.) * y * (1 - z) * kSuperControlPoints[3] +
+          (x < 0.5 ? (2. * x) : (2. - 2. * x)) * y * (1 - z) *
+              kSuperControlPoints[4] +
+          (x < 0.5 ? 0. : 2 * x - 1) * y * (1 - z) * kSuperControlPoints[5] +
+          (x < 0.5 ? (1 - 2. * x) : 0.) * (1 - y) * z * kSuperControlPoints[6] +
+          (x < 0.5 ? (2. * x) : (2. - 2. * x)) * (1 - y) * z *
+              kSuperControlPoints[7] +
+          (x < 0.5 ? 0. : 2 * x - 1.) * (1 - y) * z * kSuperControlPoints[8] +
+          (x < 0.5 ? (1 - 2. * x) : 0.) * y * z * kSuperControlPoints[9] +
+          (x < 0.5 ? (2. * x) : (2. - 2. * x)) * y * z *
+              kSuperControlPoints[10] +
+          (x < 0.5 ? 0. : 2 * x - 1) * y * z * kSuperControlPoints[11];
+    }
+    return evaluations;
+  }
+};
+
+int main() {
+#ifdef ENABLE_OPEN_MP_PARALLEL
+  omp_set_num_threads(4);
+#endif
+  // Initialize generator
+  auto micro_structure_generator =
+      MicrostructureGenerator<CrossTile3D, RingSegments3D, ValueFieldExample>{};
+  // Modify the deformation function
+  micro_structure_generator.deformation_function_generator.SetNumberOfSegments(
+      std::array<int, 3>{2,2,2});
+
+  // Construct the composition
+  const auto test_composition =
+      micro_structure_generator.ComposeMicrostructureAndDerivatives();
+
+  // Export the MS and its derivatives
+  utils::Export::GuessByExtension(test_composition[0],
+                                  "composed_microstructure.itd");
+// #ifdef ENABLE_OPEN_MP_PARALLEL
+// #pragma omp parallel for
+// #endif
+//   for (std::size_t i_deriv = std::size_t{};
+//        i_deriv < ValueFieldExample::kNumberOfSuperParameters; i_deriv++) {
+//     utils::Export::GuessByExtension(test_composition[i_deriv + 1],
+//                                     std::string("composed_microstructure_") +
+//                                         std::to_string(i_deriv) +
+//                                         std::string(".itd"));
+//   }
+
+  // /// Second part
+  // // Build some random CrossTile3D
+
+  // using ADT = utils::computational_derivation::AlgoDiffType<double>;
+  // std::array<ADT, 7ul> evaluations{
+  //     ADT(.5, 1, 0),   ADT(1., 1, 0),  ADT(.3, 1, 0), ADT(1.0, 1, 0),
+  //     ADT(0.25, 1, 0), ADT(0.3, 1, 0), ADT(0.3, 1, 0)};
+  // auto microtile = CrossTile3D::GenerateMicrostructureDerivatives(evaluations);
+  // utils::Export::GuessByExtension(microtile[0], "microtile3D.xml");
+
+  // // Generate some closing tile
+  // std::array<ADT, 6ul> face_evaluations{ADT(1., 1, 0),  ADT(.3, 1, 0),
+  //                                       ADT(1.0, 1, 0), ADT(0.25, 1, 0),
+  //                                       ADT(0.3, 1, 0), ADT(0.3, 1, 0)};
+  // auto closing_tileMIN = CrossTile3D::GenerateMicrostructureClosingTile(
+  //     CrossTile3D::ClosingFace::X_MIN, face_evaluations);
+  // utils::Export::GuessByExtension(closing_tileMIN[0],
+  //                                 "closingmicrotile3Dxmin.xml");
+  // auto closing_tileMAX = CrossTile3D::GenerateMicrostructureClosingTile(
+  //     CrossTile3D::ClosingFace::X_MAX, face_evaluations);
+  // utils::Export::GuessByExtension(closing_tileMAX[0],
+  //                                 "closingmicrotile3Dxmax.xml");
+  // auto DeformationFunctionGenerator =
+  //     RingSegments3D()
+  //         .SetInnerRadius(2.)
+  //         .SetDepth(1.)
+  //         .SetOuterRadius(4.)
+  //         .SetNumberOfSegments(std::array<int, 3>{2, 2, 2});
+  // auto deformation_function = DeformationFunctionGenerator.Create();
+  // utils::Export::GuessByExtension(deformation_function,
+  //                                 "deformation_function.xml");
+
+  return 0;
+}
---
+++ example/ParametrizedMicrostructure3D/microstructure_generator.hpp
@@ -0,0 +1,218 @@
+#ifndef EXAMPLE_PARAMETRIZED_MICROSTRUCTURE3D_MICROSTRUCTURE_GENERATOR_HPP
+#define EXAMPLE_PARAMETRIZED_MICROSTRUCTURE3D_MICROSTRUCTURE_GENERATOR_HPP
+
+#ifdef ENABLE_OPEN_MP_PARALLEL
+#include "omp.h"
+#endif
+
+/**
+ * @brief Generator of the parametrized microstructure
+ *
+ * Constructs the parametrized microstructure along with its derivatives
+ *
+ * @tparam Microtile            Microtile Example (Parametrized function)
+ * @tparam DeformationFunction  Deformation function as a BezierGroup
+ * @tparam ValueField           Value function that is used to determine the
+ * local Microtileparameters
+ */
+template <typename Microtile, typename DeformationFunction, typename ValueField>
+class MicrostructureGenerator {
+ private:
+  // Aliases
+  using ADT = utils::computational_derivation::AlgoDiffType<double>;
+  using PointADT3D = Point<3, ADT>;
+  using Point3D = Point<3, double>;
+  using BezierGroup = BezierSplineGroup<3, Point3D, double>;
+  using Bezier = BezierSpline<3, Point3D, double>;
+
+  // TransformPoints_ transforms the points provided by
+  // Microtile::kEvaluationPoints these pints are transformed by a linear
+  // interpolation funciton and set into the Valuefield function to retrieve
+  // the local parameters  of the microtile function.
+  template <std::size_t number_of_points>
+  std::array<Point3D, number_of_points> TransformPoints_(
+      const Point3D& cornermin, const Point3D& cornermax,
+      const std::array<Point3D, number_of_points>& evaluation_points) const {
+    std::array<Point3D, number_of_points> return_value{};
+    const auto difference = cornermax - cornermin;
+    for (std::size_t i{}; i < number_of_points; i++) {
+      return_value[i] =
+          cornermin + Point3D{evaluation_points[i][0] * difference[0],
+                              evaluation_points[i][1] * difference[1],
+                              evaluation_points[i][2] * difference[2]};
+    }
+    return return_value;
+  }
+
+ public:
+  // Instance of the value field function, MicrotileGenerator and
+  // DeformationFunction, they should be publicly available, to facilitate their
+  // modification
+  DeformationFunction deformation_function_generator{};
+  ValueField value_field{};
+  Microtile microtile{};
+
+  // Compose microstructure
+  std::vector<BezierGroup> ComposeMicrostructureAndDerivatives() const {
+    // Loop over the externnal splines in the group
+    const BezierGroup deformation_function =
+        deformation_function_generator.Create();
+
+    // Write it out (if required)
+    utils::Export::GuessByExtension(deformation_function,
+                                    "deformation_function_generator.xml");
+
+    // Initialize return value
+    std::vector<BezierGroup> return_value(
+        ValueField::kNumberOfSuperParameters + 1u,
+        BezierGroup(Microtile::kNumberOfSplines * deformation_function.size()));
+
+    // Every spline in the mirostructure defines a specific range of the
+    // total spline group. We consider an even splitting, meaning that that
+    // the parametric sections have the same size. As a first step, the
+    // current field needs to be identified.
+    const double dx =
+        1. / static_cast<double>(
+                 deformation_function_generator.GetNumberOfSegments()[0]);
+    const double dy =
+        1. / static_cast<double>(
+                 deformation_function_generator.GetNumberOfSegments()[1]);
+    const double dz =
+        1. / static_cast<double>(
+                 deformation_function_generator.GetNumberOfSegments()[2]);
+
+#ifdef ENABLE_OPEN_MP_PARALLEL
+#pragma omp parallel for
+#endif
+    for (std::size_t i_def_x = 0;
+         i_def_x < deformation_function_generator.GetNumberOfSegments()[0];
+         i_def_x++) {
+      for (std::size_t i_def_y{};
+           i_def_y < deformation_function_generator.GetNumberOfSegments()[1];
+           i_def_y++) {
+        for (std::size_t i_def_z{};
+             i_def_z < deformation_function_generator.GetNumberOfSegments()[2];
+             i_def_z++) {
+          std::vector<BezierGroup> microtile_vector;
+          if constexpr (Microtile::HAS_CLOSING_TILE_DEFINITION) {
+            // If a closing tile is defined we can create watertight structures
+            if (i_def_y == 0) {
+              const auto transformed_points = TransformPoints_(
+                  Point3D{i_def_x * dx, i_def_y * dy, i_def_z * dz},
+                  Point3D{(i_def_x + 1) * dx, (i_def_y + 1) * dy,
+                          (i_def_z + 1) * dz},
+                  Microtile::kClosingTileEvaluationPoints);
+              // Evaluate microtile based on the generator provided by
+              microtile_vector = Microtile::GenerateMicrostructureClosingTile(
+                  Microtile::ClosingFace::X_MAX,
+                  value_field.Evaluate(transformed_points));
+            } else if (i_def_x ==
+                       deformation_function_generator.GetNumberOfSegments()[1] -
+                           1) {
+              const auto transformed_points = TransformPoints_(
+                  Point3D{i_def_x * dx, i_def_y * dy, i_def_z * dz},
+                  Point3D{(i_def_x + 1) * dx, (i_def_y + 1) * dy,
+                          (i_def_z + 1) * dz},
+                  Microtile::kClosingTileEvaluationPoints);
+              // Evaluate microtile based on the generator provided by
+              microtile_vector = Microtile::GenerateMicrostructureClosingTile(
+                  Microtile::ClosingFace::X_MAX,
+                  value_field.Evaluate(transformed_points));
+            } else {
+              // If no closing tile is defined, then we set normal tiles in the
+              // boundary elements
+              const auto transformed_points = TransformPoints_(
+                  Point3D{i_def_x * dx, i_def_y * dy, i_def_z * dz},
+                  Point3D{(i_def_x + 1) * dx, (i_def_y + 1) * dy,
+                          (i_def_z + 1) * dz},
+                  Microtile::kEvaluationPoints);
+              // Evaluate microtile based on the generator provided by
+              microtile_vector = Microtile::GenerateMicrostructureDerivatives(
+                  value_field.Evaluate(transformed_points));
+            }
+          } else {
+            // If no closing tile is defined, then we set normal tiles in the
+            // boundary elements
+            const auto transformed_points = TransformPoints_(
+                Point3D{i_def_x * dx, i_def_y * dy, i_def_z * dz},
+                Point3D{(i_def_x + 1) * dx, (i_def_y + 1) * dy,
+                        (i_def_z + 1) * dz},
+                Microtile::kEvaluationPoints);
+            // Evaluate microtile based on the generator provided by Microtile
+            microtile_vector = Microtile::GenerateMicrostructureDerivatives(
+                value_field.Evaluate(transformed_points));
+          }
+
+          // Compose the microstructure in first position of vector
+          const std::size_t combined_index =
+              i_def_x +
+              deformation_function_generator.GetNumberOfSegments()[0] *
+                  i_def_y +
+              deformation_function_generator.GetNumberOfSegments()[0] *
+                  deformation_function_generator.GetNumberOfSegments()[1] *
+                  i_def_z;
+
+          // Construct the actual Composition (microstructure)
+          const auto composed_microstructure_tile =
+              deformation_function[combined_index].Compose(microtile_vector[0]);
+          for (unsigned i_spline_in_tile{};
+               i_spline_in_tile < Microtile::kNumberOfSplines;
+               i_spline_in_tile++) {
+            return_value[0][combined_index * Microtile::kNumberOfSplines +
+                            i_spline_in_tile] =
+                composed_microstructure_tile[i_spline_in_tile];
+          }
+
+          /// Construct the corresponding derivatives
+          // Precompute the derivatives of the deformation function as they are
+          // required multiple times (for every internal control points of the
+          // value field funciton)
+          // Here it is hard-coded for the two dimensional case
+          auto microstructure_derivative_first_par_dim =
+              deformation_function[combined_index]
+                  .DerivativeWRTParametricDimension(0)
+                  .Compose(microtile_vector[0]);
+          auto microstructure_derivative_second_par_dim =
+              deformation_function[combined_index]
+                  .DerivativeWRTParametricDimension(1)
+                  .Compose(microtile_vector[0]);
+          auto microstructure_derivative_third_par_dim =
+              deformation_function[combined_index]
+                  .DerivativeWRTParametricDimension(2)
+                  .Compose(microtile_vector[0]);
+          for (std::size_t i_derivative{};
+               i_derivative < value_field.kNumberOfSuperParameters;
+               i_derivative++) {
+            const auto microstructure_derivative =
+                (microstructure_derivative_first_par_dim.MultiplyComponentwise(
+                     microtile_vector[i_derivative].ExtractDimension(0)))
+                    .AddComponentwise(
+                        microstructure_derivative_second_par_dim
+                            .MultiplyComponentwise(
+                                microtile_vector[i_derivative].ExtractDimension(
+                                    1)))
+                    .AddComponentwise(
+                        microstructure_derivative_third_par_dim
+                            .MultiplyComponentwise(
+                                microtile_vector[i_derivative].ExtractDimension(
+                                    2)));
+            // Assign to microstructure
+            for (unsigned i_spline_in_tile{};
+                 i_spline_in_tile < Microtile::kNumberOfSplines;
+                 i_spline_in_tile++) {
+              return_value[i_derivative + 1]
+                          [combined_index * Microtile::kNumberOfSplines +
+                           i_spline_in_tile] =
+                              microstructure_derivative[i_spline_in_tile];
+            }
+          }
+        }
+      }
+    }
+    return return_value;
+  }
+
+  // Compose Derivatives
+};
+
+#endif  // EXAMPLE_PARAMETRIZED_MICROSTRUCTURE3D_MICROSTRUCTURE_GENERATOR_HPP

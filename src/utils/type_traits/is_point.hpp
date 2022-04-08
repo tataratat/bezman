1ec8a3a941956514a3144a6a829832a789eada36
jzwar <zwar@ilsb.tuwien.ac.at>
2022-04-08 13:00:44 +0200

    Added Identifier routines
    isPoint and isBezier checkers are added to prepare the use of wrappers

---
+++ src/utils/type_traits/is_point.hpp
@@ -0,0 +1,28 @@
+#ifndef UTILS_TYPE_TRAITS_IS_POINT_HPP
+#define UTILS_TYPE_TRAITS_IS_POINT_HPP
+
+#include "bezierManipulation/src/point.hpp"
+
+namespace beziermanipulation::utils::type_traits
+{
+  
+/// Checker if a template type is an instance of Point
+template <typename PointType>
+struct isPoint {
+  constexpr static bool value = false;
+};
+
+/// Checker if a template type is an instance of Point
+template <unsigned int spatial_dimension, typename BaseType>
+struct isPoint<Point<spatial_dimension, BaseType>> {
+  constexpr static bool value = true;
+};
+
+/// Alias std naming conform
+template <typename PointType>
+inline constexpr bool isPoint_v = isPoint<PointType>::value;
+
+} // namespace beziermanipulation::utils::type_traits
+
+
+#endif  // UTILS_TYPE_TRAITS_IS_POINT_HPP

8ab5113277bfe1c3f05bc2c0421cfc9715584e21
Thurner, Lukas <lukas.thurner@tuwien.ac.at>
2022-05-05 13:58:44 +0200

    HyperCube add sub-function & remove namespace uniquify

--- src/utils/export.hpp
+++ src/utils/export.hpp
@@ -10,7 +10,7 @@
 #include "bezierManipulation/src/point.hpp"
 #include "bezierManipulation/src/utils/algorithms/int_power.hpp"
 #include "bezierManipulation/src/utils/base64.hpp"
-#include "bezierManipulation/src/utils/uniquify/point_uniquifier.hpp"
+#include "bezierManipulation/src/utils/algorithms/point_uniquifier.hpp"
 
 namespace beziermanipulation::utils {
 
